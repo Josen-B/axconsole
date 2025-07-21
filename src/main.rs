1a4e2c0e4da023b933f3988d0060d66fb49c7a2f
Josen-B <baiqiaosen@gmail.com>
2025-07-20 19:17:54 -0600

    [test] pubulish to crates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-#![cfg_attr(feature = "axstd", no_std)]
-#![cfg_attr(feature = "axstd", no_main)]
-
-#[cfg(feature = "axstd")]
-use axstd::println;
-use axstd::thread::sleep;
-use axstd::time::Duration;
-
-#[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
-    loop{
-        println!("Hello, world!");
-        sleep(Duration::from_secs(3));
-    }
+    println!("Hello, world!");
 }
